bec4cf1017bad6387671797a2afa8712339a3ad8
t-rakuyama <t.rakuyama929@gmail.com>
2024-02-12 00:07:13 +0900

    レイアウトファイルを作成

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
