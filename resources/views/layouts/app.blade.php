632a946acc8aab68510eeb831ef5f6324a89da7d
t-rakuyama <t.rakuyama929@gmail.com>
2024-02-12 09:21:11 +0900

    fixup! レイアウトファイルを作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,10 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        @section('js')
+        @show
+        @section('css')
+        @show
     </head>
     <body>
         <div class="container">
