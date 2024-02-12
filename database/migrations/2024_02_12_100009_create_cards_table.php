5e4a0905bf15132d152d45f7deee891e86572e50
t-rakuyama <t.rakuyama929@gmail.com>
2024-02-12 11:14:04 +0900

    migrationファイル作成

--- database/migrations/2024_02_12_100009_create_cards_table.php
+++ database/migrations/2024_02_12_100009_create_cards_table.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('cards', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained();
+            $table->string('cellNumber');
+            $table->string('value');
+            $table->boolean('isHit')->default(false);
         });
     }
 
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('cards');
     }
 };
