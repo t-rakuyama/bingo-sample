2fbc22159a48dcfa058869d8eae6cdd356edae5c
t-rakuyama <t.rakuyama929@gmail.com>
2024-02-12 13:54:18 +0900

    userを作成する処理作成

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Repositories;
+
+use App\Models\User;
+
+class UserRepository
+{
+    public function createUser(string $name)
+    {
+        $user = new User();
+        $user->fill(['name' => $name])->save();
+        return $user
+    }
+}
