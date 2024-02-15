8a36bec16b2457c331bda30c745f06f6a82b93ba
t-rakuyama <t.rakuyama929@gmail.com>
2024-02-15 23:54:58 +0900

    controllerからServiceを呼び出すように修正

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,35 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Repositories\UserRepository;
-use App\Repositories\CardRepository;
+
 use App\Services\DrawCard;
+use App\Services\ForwardGame;
 
 class GameController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $userRepository = new UserRepository();
-        $users = $userRepository->find();
-        $cardRepository = new CardRepository();
-        $cards = $cardRepository->find();
-
-        $data = [];
-        foreach($users as $user) {
-            $pData = ['name' => $user->name];
-            $cardData = [];
-
-            foreach ($cards as $key => $card) {
-                if($card->user_id === $user->id) {
-                    array_push($cardData, ['value' => $card->value, 'isHit' => $card->isHit]);
-                }
-            }
-            $pData['card'] = array_chunk($cardData, 5);
-            array_push($data, $pData);
-        }
+        $forwardGame = new ForwardGame();
+        $data = $forwardGame->forwardGame();
         $number = session('number');
-        return view('game.index', ['data' => $data, 'number' => $number]);
+        $isFinish = count(session('numbers')) === 0;
+        return view('game.index', ['data' => $data, 'number' => $number, 'isFinish' => $isFinish ]);
     }
 
     public function post()
